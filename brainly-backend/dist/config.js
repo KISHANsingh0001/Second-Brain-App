a4e55cde522efc897495c4f83c5aa81478d11169
KISHAN SINGH THAKUR <126978912+KISHANsingh0001@users.noreply.github.com>
2025-08-05 18:03:15 +0530

    Update config.js

--- brainly-backend/dist/config.js
+++ brainly-backend/dist/config.js
@@ -1,3 +1,2 @@
 "use strict";
-// export const JWT_SECRET = "ILOVEBMWS12334554hfjdkuedjsdhe";
-// export const MONGO_URL = "mongodb+srv://kishansinghthakur27:GuQ4OM7AiKlAO5I8@cluster0.k0ymi.mongodb.net/Second-brain";
+
