c6a8cf92db1a3d83d9e87fec372ca97503c2fa98
Kishan <kishansinghthakur27@gmail.com>
2025-08-06 23:40:16 +0530

    done

--- brainly-frontend/src/config.ts
+++ brainly-frontend/src/config.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://second-brain-app-production-6fee.up.railway.app";
-export const BACKEND_URL = "http://localhost:3003";
+export const BACKEND_URL = "https://second-brain-app-production-6fee.up.railway.app";
+// export const BACKEND_URL = "http://localhost:3003";
